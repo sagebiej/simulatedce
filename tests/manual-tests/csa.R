b84353073b43f5db1a049ee7a57617976ce7bfd3
Julian Sagebiel <julian.sagebiel@idiv.de>
2024-04-25 22:37:55 +0200

    changed test/csa with new designs

--- tests/manual-tests/csa.R
+++ tests/manual-tests/csa.R
@@ -8,8 +8,16 @@ devtools::load_all()
 
 set.seed(3393)
 
-designpath<- system.file("extdata","CSA" ,package = "simulateDCE")
-#notes <- "This design consists of different heuristics. One group did not attend the methan attribute, another group only decided based on the payment"
+designpath<- system.file("extdata","CSA","linear" ,package = "simulateDCE")
+
+library(rlang)
+library(formula.tools)
+
+
+
+
+
+
 
 notes <- "No Heuristics"
 
@@ -19,25 +27,26 @@ nosim=2 # number of simulations to run (about 500 is minimum)
 destype <- "spdesign"
 
 bcoeff<-list(
-  bx1 = -0.1,
-  bx2 = -0.1,
-  bx3 = -0.05,
-  bx4 = -0.025)
+  bx1 = -0.02,
+  bx2 = -0.03,
+  bx3 = -0.02,
+  bx4 = -0.005,
+  bnobuy = -1)
 
 #place your utility functions here
 ul<- list(u1= list(
   v1 =V.1 ~  bx1 * alt1.x1 + bx2 * alt1.x2 +  bx3 * alt1.x3  +  bx4 * alt1.x4,
   v2 =V.2 ~  bx1 * alt2.x1 + bx2 * alt2.x2 +  bx3 * alt2.x3  +  bx4 * alt2.x4,
-  v3 =V.3 ~ -5
+  v3 =V.3 ~ bnobuy
 )
 )
 
 
 
 csa <- simulateDCE::sim_all(nosim = nosim, resps=resps, destype = destype,
-                 designpath = designpath, u= ul, bcoeff = bcoeff)
+                            designpath = designpath, u= ul, bcoeff = bcoeff)
 
 
 
-saveRDS(csa,file = "tests/manual-tests/csa.RDS")
+
 
